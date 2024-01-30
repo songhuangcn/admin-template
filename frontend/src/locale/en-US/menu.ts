bdc2748707b15ac417071cf34d64ea2866f76a28
Song Huang <song@hdgcs.com>
2024-01-30 15:10:36 +0800

    docs: add multiple-language docs

--- frontend/src/locale/en-US/menu.ts
+++ frontend/src/locale/en-US/menu.ts
@@ -2,6 +2,6 @@ export default {
   'menu.dashboard': 'Dashboard',
   'menu.dashboard.workplace': 'Workplace',
   'menu.system-management': 'System Management',
-  'menu.system-management.user': 'Users Management',
-  'menu.system-management.role': 'Roles Management',
+  'menu.system-management.user': 'User Management',
+  'menu.system-management.role': 'Role Management',
 };
